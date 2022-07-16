54faf7bd7fc339e2f4170a6480576c07e6e6c2be
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-16 14:27:21 +0530

    done

--- block-BNaabo/server.js
+++ block-BNaabo/server.js
@@ -1,26 +1,55 @@
-var express = require('express')
+// var express = require('express')
 
-var app = express();
+// var app = express();
 
-app.use((req,res,next) => {
-  console.log(req.method,req.url);
-  next();
-});
+// app.use((req,res,next) => {
+//   console.log(req.method,req.url);
+//   next();
+// });
+
+// app.use(express.json());
+
+// app.use(express.urlencoded({extened: false}))
+
+// app.use(express.static(__dirname + 'public'))
+
+// app.get('/',(req,res) => {
+//   res.sendFile(__dirname + "/html/index.html");
+// });
+
+// app.post('/json',(req,res) => {
+//   console.log(req.body);
+// })
+
+// app.listen(3000,() => {
+//   console.log('server listening on port 3k')
+// }); 
+
+var express = require('express');
+var app = express();  
+
+// middleware
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(__dirname + "/public"))
 
-app.use(express.urlencoded({extened: false}))
+app.get('/json',(req,res)=>{
+  res.sendFile(__dirname + "/index.html")
+})
 
-app.use(express.static(__dirname + 'public'))
+app.get('/', (req,res)=>{
+  res.send('Welcome to Express')
+})
 
-app.get('/',(req,res) => {
-  res.sendFile(__dirname + "/html/index.html");
-});
-
-app.post('/json',(req,res) => {
+app.post('/',(req,res)=>{
   console.log(req.body);
 })
 
-app.listen(3000,() => {
-  console.log('server listening on port 3k')
-}); 
+app.post('/',(req,res)=>{
+  console.log(req.body)
+}) 
+
+app.listen(4000,()=>{
+  console.log('server is listening on port 4k')
+})

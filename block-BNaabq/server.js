021c6010d4f4d251859a7c5547b56e18cc05a8be
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-18 13:28:45 +0530

    Refactor code

--- block-BNaabq/server.js
+++ block-BNaabq/server.js
@@ -6,40 +6,36 @@ var cookieParser   = require('cookie-parser')
 
 var app = express(); 
 
-// app.use((req,res,next)=>{
-//  console.log(req.method,req.url);
-//  next();
-// })
 
 
-
-app.use(logger('dev')); 
+app.use(express.json())
+app.use(express.urlencoded  ({extened:false}));
+app.use(express.static(__dirname + 'public'));
 
 app.use(cookieParser());
 
-
-
-app.use(express.json());
-
-app.use(express.urlencoded  ({extened:false}));
-
-app.use(express.static(__dirname + 'public'));
+app.use(logger('combined'))
 
 app.use((req,res,next)=>{
-  var count = req.cookies.count
-  if(count){
-   res.cookie("count",Number(count) + 1)
-  }else{
-    res.cookie("count",1)
-  }
   console.log(req.cookies);
- 
-  next( )
-})    
+})
+
+
+app.use("/about",(req,res,next)=>{
+  res.cookie('username','sanaya')
+  res.send('About Page');  
+})
+
+
 
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + "/index.html")
 })
-app.listen(3000,()=>{
-  console.log('server is listening on port 3k')
+
+
+
+
+
+app.listen(4000,()=>{
+  console.log('server is listening on port 4k')
 });

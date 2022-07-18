b39c76e40a0fcca77019143d2bd1d1e08498284e
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-18 19:25:35 +0530

    Assignment-1 refactor code

--- block-BNaabv/server.js
+++ block-BNaabv/server.js
@@ -1,44 +1,59 @@
 var express = require("express");
+
+// require
+
 var app = express();
 var logger = require("morgan");
-var cookieParser = require('cookie-parser');
+var cookieparser = require('cookie-parser');
 
-app.use( logger('dev') );
+// middleware
 
-app.use(cookie-parser());
+app.use( express.json() );
+app.use( express.urlencoded( { extended : false  } ) );
+app.use( logger('dev') ); 
+app.use(cookieparser());
 
-app.get( '/user/:username', ( req, res )=>{
-  var username = req.params.username;
-  res.send(`<h1>${username}</h1>`);
-} )
+app.use((req,res,next)=>{
+  res.cookies("username","sanaya")
+  next();
+})
+
+// routes
 
 app.get( '/', ( req, res )=>{
-  res.send(`<h2>Welcome to express</h2>`);
+  res.send(`<h2>Welcome to express</h2>` );
 } );
 
-app.get('/',(req,res)=>{
+app.get('/about',(req,res)=>{
   res.send('My name is sanaya')
 })
 
-app.use( express.urlencoded( { extended : false  } ) );
 
 app.post( '/form', ( req, res )=>{
-    res.send( req.body );
+    res.json( req.body );
 } );
 
-app.use( express.json() );
 
 app.post( '/json', ( req, res )=>{
-    res.send( req.body );
+    res.json( req.body );
 } );
 
-app.use( ( req, res, next ) => {
-    res.status(404).send("<h1>Page not found on the server</h1>");
-} );
+app.get('/user/:username',(req,res)=>{
+  var username = req.params.username;
+  res.send(`<h2>${username}  <h2>`)
+})
 
-app.use( ( req, res, next ) => {
-    res.status(500).send("<h1>Client and Server error</h1>");
-} );
+// 404 middleware
+
+app.use((req,res,next)=>{
+  res.send("Page Not Found ")
+})
+
+// custom middleware
+
+app.use((err,req,res,next)=>{
+  res.send(err);
+})
 
 app.listen( 3000, ()=>{
   console.log("server listening on port 3000");
